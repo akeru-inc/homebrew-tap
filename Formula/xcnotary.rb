8b083984467f05b211ec2af2801244812475b19f
David Vartan <davidvartan@users.noreply.github.com>
2020-04-10 05:36:54 -0700

    xcnotary v0.3.0

--- Formula/xcnotary.rb
+++ Formula/xcnotary.rb
@@ -1,8 +1,8 @@
 class Xcnotary < Formula
   desc "macOS app notarization helper"
   homepage "https://github.com/akeru-inc/xcnotary"
-  url "https://github.com/akeru-inc/xcnotary/archive/v0.2.0.zip"
-  sha256 "0a4c8d96b125dafb738498e8b718c3261bec1dacab9b614994b41de4812a1ef3"
+  url "https://github.com/akeru-inc/xcnotary/archive/v0.3.0.zip"
+  sha256 "6d481eb35384dbcfaedc6ed4728de80270c3cc6e6b56776f3ca9eb15fe30fbfc"
 
   depends_on "rust" => :build
 
@@ -11,9 +11,9 @@ class Xcnotary < Formula
   end
 
   bottle do
-    root_url "https://github.com/akeru-inc/xcnotary/releases/download/v0.2.0"
+    root_url "https://github.com/akeru-inc/xcnotary/releases/download/v0.3.0"
     cellar :any_skip_relocation
-    sha256 "0fae5324dd1785f89c7fad13f9a326e0be673a3e8563179aa6c69a778c8d4716" => :catalina
+    sha256 "0bc97ec8b51b245baa4fda6e3058b3ec30f59c3f88a0cec7985df870dad569f2" => :catalina
   end
 
   test do
