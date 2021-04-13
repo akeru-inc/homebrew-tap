0fd238c7dff14c50b0477b9a64641450d265b981
David Vartan <davidvartan@users.noreply.github.com>
2021-04-12 19:53:27 -0700

    Update bottle format.

--- Formula/xcnotary.rb
+++ Formula/xcnotary.rb
@@ -4,17 +4,16 @@ class Xcnotary < Formula
   url "https://github.com/akeru-inc/xcnotary/archive/v0.4.6.zip"
   sha256 "fc1908a43f21250611cd36b3bdcfb96996feb7c6d1fcef492de25fa40ddeca9e"
 
+  bottle do
+    root_url "https://github.com/akeru-inc/xcnotary/releases/download/v0.4.6"
+    sha256 cellar: :any_skip_relocation, mojave:   "d798b49c6cec0a6ea2528dc9dcf3aa41fd5eabe0ecdef807008667d010ab1368"
+    sha256 cellar: :any_skip_relocation, catalina: "d798b49c6cec0a6ea2528dc9dcf3aa41fd5eabe0ecdef807008667d010ab1368"
+  end
+
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
-  end
-
-  bottle do
-    root_url "https://github.com/akeru-inc/xcnotary/releases/download/v0.4.6"
-    cellar :any_skip_relocation
-    sha256 "d798b49c6cec0a6ea2528dc9dcf3aa41fd5eabe0ecdef807008667d010ab1368" => :mojave
-    sha256 "d798b49c6cec0a6ea2528dc9dcf3aa41fd5eabe0ecdef807008667d010ab1368" => :catalina
   end
 
   test do
