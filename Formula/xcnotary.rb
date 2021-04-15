b21c4c92e7fb17e9b9b5017b5a684924b66d4cd4
David Vartan <davidvartan@users.noreply.github.com>
2021-04-14 21:02:35 -0700

    xcnotary v0.4.7

--- Formula/xcnotary.rb
+++ Formula/xcnotary.rb
@@ -1,15 +1,15 @@
 class Xcnotary < Formula
   desc "macOS app notarization helper"
   homepage "https://github.com/akeru-inc/xcnotary"
-  url "https://github.com/akeru-inc/xcnotary/archive/v0.4.6.3.zip"
-  sha256 "699355770c8d53fd391abe49021e999e3a8f9a0a12143a9e9895a33b5652bf52"
+  url "https://github.com/akeru-inc/xcnotary/archive/v0.4.7.zip"
+  sha256 "96842db34b1eb8ee6862e16408d27d7fabe5b81cf244795ab5d6e4d03ee90e21"
 
   bottle do
-    root_url "https://github.com/akeru-inc/xcnotary/releases/download/v0.4.6.3"
-    sha256 cellar: :any_skip_relocation, mojave:   "c2cf54ef129511e7fa4d9995524fbda7ce43123d1e694fc164ab9a49d9851a70"
-    sha256 cellar: :any_skip_relocation, catalina: "c2cf54ef129511e7fa4d9995524fbda7ce43123d1e694fc164ab9a49d9851a70"
-    sha256 cellar: :any_skip_relocation, big_sur:  "c2cf54ef129511e7fa4d9995524fbda7ce43123d1e694fc164ab9a49d9851a70"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c2cf54ef129511e7fa4d9995524fbda7ce43123d1e694fc164ab9a49d9851a70"
+    root_url "https://github.com/akeru-inc/xcnotary/releases/download/v0.4.7"
+    sha256 cellar: :any_skip_relocation, mojave:   "c6e4444b04388abfec26aa4d9ceba99e65626d7577491533f0bf1a8a705f4a23"
+    sha256 cellar: :any_skip_relocation, catalina: "c6e4444b04388abfec26aa4d9ceba99e65626d7577491533f0bf1a8a705f4a23"
+    sha256 cellar: :any_skip_relocation, big_sur:  "c6e4444b04388abfec26aa4d9ceba99e65626d7577491533f0bf1a8a705f4a23"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c6e4444b04388abfec26aa4d9ceba99e65626d7577491533f0bf1a8a705f4a23"
   end
 
   depends_on "rust" => :build
